eeb5ef72b443a2b191190410343d5a0a81fc4a96
nadyapost <nadyapostr@gmail.com>
2019-10-21 21:54:47 +1100

    Initial Commit

---
+++ We Split/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  We Split
+//
+//  Created by Nadya Postriganova on 21/10/19.
+//  Copyright © 2019 Nadya Postriganova. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
