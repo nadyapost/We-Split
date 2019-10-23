7f86ec0a9fdea9097e586b204c17f75fa287da2f
nadyapost <nadyapostr@gmail.com>
2019-10-23 22:04:18 +1100

    Introduce form view

--- We Split/ContentView.swift
+++ We Split/ContentView.swift
@@ -9,10 +9,74 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var billAmount = ""
+    @State private var numberOfPeople = ""
+    @State private var tipPercenage = 2
+//    @State private var grandTotal: Double = 0
+//    @State private var grandTotalString = ""
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople) ?? 1
+        let tipSelection = Double(tipPercentages[tipPercenage])
+        let orderAmount = Double(billAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+
+        return amountPerPerson
+    }
+    var grandTotal: Double {
+        let tipSelection = Double(tipPercentages[tipPercenage])
+        let orderAmount = Double(billAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+
+        return grandTotal
+    }
+    
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Bill amount", text: $billAmount)
+                        .keyboardType(.decimalPad)
+                    TextField("Number of people", text: $numberOfPeople)
+                        .keyboardType(.numberPad)
+//                    Picker("Number of people", selection: $numberOfPeople) {
+//                        ForEach(2 ..< 4) {
+//                            Text("\($0) people")
+//                        }
+//                    }
+                }
+                Section(header: Text("How much tip do you want to leave?")) {
+                    Picker("Tip Percentage", selection: $tipPercenage) {
+                        ForEach(0..<tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                Section(header: Text("Total amount")) {
+                    Text("$\(grandTotal, specifier: "%.2f")")
+                }
+                Section(header: Text("Amount per person")) {
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                        .foregroundColor(.red)
+                }
+                
+            }
+            .navigationBarTitle("We Split")
+        }
+        
+        
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
